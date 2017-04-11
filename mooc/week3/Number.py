b821c1a8d503d47ade730d460975a1c8c226ecae
LarryKoo <gumutianqi@gmail.com>
2017-04-11 13:54:25 +0800

    第三周：Python 基本数据类型

---
+++ mooc/week3/Number.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+print(sys.float_info)
+
+z = 1.23e-4+5.6e+89j
+
+print(z)
+print(z.real)
+print(z.imag)
+
+
+print(1+0.45)
+
+print("--------------")
+print(int(1.234))
+print(float(1234))
+print(complex(3))
+
+print(type(4.5))
+
+print("--------------")
+print(1+2)
+print(1-2)
+print(1*2)
+print(1/2)
+print(1%2)
+print(2**3)
+print(pow(2,3))
---
+++ mooc/week3/String.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+## 字符串类型
+
+str1 = "Hello John"
+
+print(type(str1))
+
+print("\\\"12\\4")
+
+print(str1[0])
+print(str1[6])
+print(str1[-1])
+
+# str[start, end]
+print(str1[0:3])
+
+print(len(str1))
+
+print((str1 + "; ") * 3)
+
+print(str(1.23))
